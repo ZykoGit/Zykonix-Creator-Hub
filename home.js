5b3c5b20f68a87e40ca5a9098afd416342b11da1
Zyko <micaiahejardin@gmail.com>
2026-03-16 17:54:21 -0500

    Update home.js

--- home.js
+++ home.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 const supabase = createClient(
   "https://edyaswzqtwzczqjwnrtj.supabase.co",
-  "YOUR_SUPABASE_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeWFzd3pxdHd6Y3pxanducnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1ODE5NDcsImV4cCI6MjA4ODE1Nzk0N30.o8WlHRvrSZmfwjPGD-1Q0VEOwrHR7WBa92Uctp-xL_I"
 );
 
 // Check session
